4f70b73b755bb1418e07d70895ea72c13a9a09b6
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-11 14:49:51 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -23,7 +23,10 @@ def play(songs)
   puts "Please enter a song name or number:"
   ans = gets.strip
   
-  
+  temp = ans.to_i
+  if temp <= songs.length
+    puts "Playing #{songs[temp-1]}"
+  end
   
   
   x = 0
@@ -34,10 +37,7 @@ def play(songs)
     x += 1
   end
   
-  temp = ans.to_i
-  if temp <= songs.length
-    puts "Playing #{songs[temp-1]}"
-  end
+  
   puts "Invalid input, please try again"
   
 end
